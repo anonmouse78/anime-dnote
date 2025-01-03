527bc29058293c852384d8e1e7e00f9b28210d6b
Anøn <electrofusion78@proton.me>
2025-01-03 16:16:11 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('donate-button').addEventListener('click', function() {
-    var menu = document.getElementById('crypto-menu');
-    menu.classList.toggle('hidden');
-});
