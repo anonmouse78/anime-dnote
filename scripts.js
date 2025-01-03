ab1565c9d599176863eb8130dd0f4abc3ed61279
Anøn <electrofusion78@proton.me>
2025-01-03 13:38:37 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('donate-button').addEventListener('click', function() {
+    var menu = document.getElementById('crypto-menu');
+    menu.classList.toggle('hidden');
+});
